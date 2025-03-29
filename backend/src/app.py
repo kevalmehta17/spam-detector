ea2df4dc4922ce8c6b35f93d1a934a68b55ba350
Keval <kevalmehta885@gmail.com>
2025-03-29 11:29:48 +0530

    fix: Applied Model & vectorizer

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,25 +1,24 @@
 from flask import Flask, jsonify, request
-import pickle
+from joblib import load
 
 app = Flask(__name__)
 
 # Load the model from the pickle file
 
-with open('models/spam_model.pkl', 'rb') as model_file:
-    model = pickle.load(model_file)
+model = load('../model/spam_classifier.joblib')
+vectorizer = load('../model/vectorizer.joblib')
 
-
-# Define the API route Endpoint
-
-@app.route('/predict', methods=['POST'])
+@app.route('/predict',methods=["POST"])
 
 def predict():
-    # get the data from the request from the client
-    data  = request.get_json() 
-    email = data['email'] #this will extract the email from the request data
-    prediction = model.predict([email])[0] 
-    result = 'Spam' if prediction == 'spam' else  'Not Spam'
-    return jsonify({'prediction':result})
+    data = request.json
+    message = data.get("message","")
+
+    message_tfidf = vectorizer.transform([message])
+    prediction = model.predict(message_tfidf)[0]
+
+    return jsonify({"predicition":"spam" if prediction == 1 else "Not-Spam"})
+
 
 if __name__ == '__main__':
-    app.run(port=5000)
+    app.run(port=5000,debug=True)
