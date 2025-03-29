8bc601d46fdb2dedf2d8410e92363ca716d8829b
Keval <kevalmehta885@gmail.com>
2025-03-29 11:15:12 +0530

    feat: Training The Model

---
+++ backend/src/train.py
@@ -0,0 +1,31 @@
+from preprocess import load_and_clean_data
+from sklearn.feature_extraction.text import TfidfVectorizer 
+from sklearn.naive_bayes import MultinomialNB #Algorithm used for the classification
+from sklearn.model_selection import train_test_split #used to split the dataset into training and testing set
+from joblib import dump #used to save the model as a pickle file ~ allow model to be saved without retraining
+
+# Load the dataset
+df = load_and_clean_data('../data/spam.csv')
+
+# Split the dataset into features and lables:-
+
+x_train, x_test, y_train, y_test = train_test_split(df['message'],df['label'],test_size=0.2, random_state=42) #80% training and 20% testing 
+
+
+vectorizer  = TfidfVectorizer() #TF-IDF:- usefull for the text-classification
+x_train_tfidf = vectorizer.fit_transform(x_train)
+x_test_tfidf = vectorizer.transform(x_test) #transform the test data using the same vectorizer
+
+# Train the model
+model = MultinomialNB() #create Native bayes classifier object
+model.fit(x_train_tfidf, y_train) #Train the model using the training data
+
+# Save model & vectorizer
+
+dump(model,"../models/spam_classifier.joblib") #save the model as a pickle file
+dump(vectorizer,"../models/vectorizer.joblib") #save the vectorizer as a pickle file
+# dump() is from the joblib library, which is used for saving and loading Python objects efficiently.
+
+
+
+print("Model and vectorizer saved successfully.")
